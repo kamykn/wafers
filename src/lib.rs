4c093a352f4179bb8d843efa8835cb9ed17bf837
kamykn <to.kamykn@gmail.com>
2018-06-07 01:23:02 +0900

    0607

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+static mut FOUND_WORD_LIST: &'static mut [&'static str] = &mut [];
+
 #[no_mangle]
-pub extern "C" fn search<'a> (inputWord: &str, searchList: Vec<&str>) -> &'a str {
-    let mut foundWordList = Vec::<&str>::new();
-    let aaa = &"Hello";
+pub extern "C" fn search(inputWord: String, searchList: &'static [&'static str]) -> *const &str {
     for inputChar in inputWord.chars() {
         let mut isFound = false;
-        for searchWord in &searchList {
+        let mut index = 0;
+        for mut searchWord in searchList.iter() {
             for searchChar in searchWord.chars() {
                 if inputChar == searchChar {
                     isFound = true;
@@ -12,14 +13,18 @@ pub extern "C" fn search<'a> (inputWord: &str, searchList: Vec<&str>) -> &'a str
             }
 
             if isFound {
-                foundWordList.push("aaaa");
+                unsafe {
+                    FOUND_WORD_LIST[index] = &searchWord;
+                }
+                index = index+1;
                 break;
             }
         }
 
     }
 
-    // &foundWordList[0]
-    aaa
+    unsafe {
+        &FOUND_WORD_LIST[0]
+    }
 }
 
