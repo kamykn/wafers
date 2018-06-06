03a826f7938143bcbd962bfcb3b05558ba5c233e
kmszk <kmszk.kent@gmail.com>
2018-06-07 01:23:02 +0900

    0607

--- js/init.js
+++ js/init.js
@@ -1,8 +1,17 @@
-fetch('/wasm-fzf/target/wasm32-unknown-unknown/debug/wasm_fzf.wasm')
+fetch('/target/wasm32-unknown-unknown/debug/wasm_fzf.wasm')
 	.then(response => response.arrayBuffer())
 	.then(bytes => WebAssembly.instantiate(bytes, {}))
 	.then(results => {
 		var fzf = results.instance.exports;
-		console.log(fzf.search("1", ["1","2","3"]));
+		const offset = fzf.search("a", ["aaaa","bbb","ccc"]);
+		console.log(results.instance.exports.memory);
+		console.log(offset);
+		const array = new Uint8Array(results.instance.exports.memory.buffer, offset, 5);
+		console.log(array);
+		let str = '';
+		// for (let i = 0; i < stringBuffer.length; i++) {
+		//     str += String.fromCharCode(stringBuffer[i]);
+		// }
+		// console.log(str);
 	});
 
