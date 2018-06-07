0fd59508abd813b42b47942f31d6c15324f9c992
kmszk <kmszk.kent@gmail.com>
2018-06-08 01:26:56 +0900

    修正

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,27 @@
+#[macro_use]
+extern crate stdweb;
+
 static mut FOUND_WORD_LIST: &'static mut [&'static str] = &mut [];
 
 #[no_mangle]
 pub extern "C" fn search(inputWord: String, searchList: &'static [&'static str]) -> *const &str {
+    let check = &inputWord;
+    js! {
+        console.log(@{check});
+    }
     for inputChar in inputWord.chars() {
-        let mut isFound = false;
         let mut index = 0;
         for mut searchWord in searchList.iter() {
             for searchChar in searchWord.chars() {
                 if inputChar == searchChar {
-                    isFound = true;
+                    unsafe {
+                        FOUND_WORD_LIST[index] = &searchWord;
+                    }
+                    index = index+1;
+                    break;
                 }
-            }
-
-            if isFound {
-                unsafe {
-                    FOUND_WORD_LIST[index] = &searchWord;
-                }
-                index = index+1;
-                break;
             }
         }
-
     }
 
     unsafe {
