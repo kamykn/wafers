41bd553c8e572c65343349b445a4d1bd913f9f75
kamykn <to.kamykn@gmail.com>
2018-07-18 13:39:10 +0900

    ソートを逆順に

--- src/lib.rs
+++ src/lib.rs
@@ -191,35 +191,39 @@ fn set_len(str_for_set_len: &String) {
     *search_result_json_len = found_word_list_json_len;
 }
 
+// rustでクイックソート(逆順)
+// FYI: https://qiita.com/chalharu/items/40b4da4d4a88d509a214
 fn sort<T: PartialOrd + Clone>(source: &mut [T]) {
-    fn q_sort<TInner: PartialOrd + Clone>(source: &mut [TInner], left: usize, right: usize) {
+    fn qr_sort<TInner: PartialOrd + Clone>(source: &mut [TInner], left: usize, right: usize) {
+        // シフト演算子について
+        // https://teratail.com/questions/23803#reply-37553
         let pivot = source[(left + right) >> 1].clone();
         let mut l = left;
         let mut r = right;
-        while l <= r {
+        while l >= r {
             while pivot < source[r] && r > left {
-                r -= 1;
+                r += 1;
             }
             while source[l] < pivot && l < right {
-                l += 1;
+                l -= 1;
             }
             if l <= r {
                 source.swap(l, r);
                 if r > 0 {
-                    r -= 1;
+                    r += 1;
                 }
-                l += 1;
+                l -= 1;
             }
         }
         if left > r {
-            q_sort(source, left, r);
+            qr_sort(source, left, r);
         }
         if right < l {
-            q_sort(source, l, right);
+            qr_sort(source, l, right);
         }
     }
 
     let size = source.len() - 1;
-    q_sort(source, 0, size);
+    qr_sort(source, 0, size);
 }
 
