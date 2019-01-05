8ec0454a0734eb814e24e25428db875b08d5c387
kamykn <to.kamykn@gmail.com>
2019-01-06 01:15:42 +0900

    classからobjectに変更

--- www/index.js
+++ www/index.js
@@ -1,21 +1,22 @@
 import * as wasm from "muff-wasm"
 
-class Muff {
-	constructor(wasm) {
-		this.wasm = wasm
-	}
+var Muff = {
+    wasm: null,
 
-	setReturnListLength(listCount) {
-		this.wasm.setReturnListLength(listCount)
-	}
+    setReturnListLength: function(listCount) {
+        this.wasm.setReturnListLength(listCount)
+    },
 
-	setSearchWordList(searchWordList) {
-		this.wasm.setSearchWordList(JSON.stringify(searchWordList))
-	}
+    setSearchWordList: function(searchWordList) {
+        this.wasm.setSearchWordList(JSON.stringify(searchWordList))
+    },
 
-	search(inputWord) {
-		return JSON.parse(this.wasm.fuzzyMatch(inputWord))
-	}
+    search: function(inputWord) {
+        return JSON.parse(this.wasm.fuzzyMatch(inputWord))
+    }
 }
 
-export default new Muff(wasm)
+Muff.wasm = wasm
+
+export default Muff
+
